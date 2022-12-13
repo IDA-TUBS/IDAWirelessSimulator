944a2e0189fd1c4f04292b4b66416c94b568e736
Alex Bendrick <bendrick@ida.ing-tu-bs.de>
2022-12-13 11:32:21 +0100

    updated scripts

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+REF=$(pwd)
+
+PATH_FILE=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $PATH_FILE
+
 read -p "Enter path to inet4 package: " -i "$HOME/" -e PATH_INET
 rm ../profile
 
 template=`cat profileTemplate`
-echo "${template//<PATH_INET>//$PATH_INET}" > ../profile
+echo "${template//<PATH_INET>/$PATH_INET}" > ../profile
+
+cd $REF
